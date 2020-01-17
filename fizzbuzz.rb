38191a6383198f158b80681a4bfeb1e0f64c4a30
Julie  McManus <julieannesays@gmail.com>
2020-01-17 00:59:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if x % 3 == 0  && i % 5 == 0
+  if x % 3 == 0  && x % 5 == 0
     puts "FizzBuzz"
   elsif x % 3 == 0
-    puts 'Fizz'
+    puts "Fizz"
   elsif  x % 5 == 0
     puts "Buzz"
   else
